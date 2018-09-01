3b5383db30ba43708c0ad320df98064219ca470d
Vijay Balasubramaniam <vbalasu@gmail.com>
2018-08-31 18:09:24 -0700

    Added comment pip install jsonrpclib-pelix

--- awsclient.py
+++ awsclient.py
@@ -1,3 +1,4 @@
+#pip install jsonrpclib-pelix
 import jsonrpclib
 server = jsonrpclib.ServerProxy('http://34.234.119.236:8080')
 print(server.add(5,6))
--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+#pip install jsonrpclib-pelix
 import jsonrpclib
 server = jsonrpclib.ServerProxy('http://127.0.0.1:8080')
 print(server.add(5,6))
